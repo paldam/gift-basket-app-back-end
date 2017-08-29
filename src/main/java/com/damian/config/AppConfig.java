6b08205370348e81b27ae20292c7bb601128e7bb
dami_dev <dami@onet.eu>
2017-08-29 23:44:07 +0200

    initial commit

---
+++ src/main/java/com/damian/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.damian.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "com.damian")
+@EnableWebMvc
+
+public class AppConfig {
+}
