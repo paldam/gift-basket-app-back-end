3a8eef0dc5d102ad398d23964c51e09c74de4cb7
dami_dev <dami@onet.eu>
2017-09-16 18:40:11 +0200

    update Rest Controller

--- src/main/java/com/damian/config/SpringSecurityConfig.java
+++ src/main/java/com/damian/config/SpringSecurityConfig.java
@@ -43,6 +43,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers(" /b").permitAll()
+                .antMatchers("/products").permitAll()
                 .antMatchers("/a").authenticated()
                 .antMatchers("/c").permitAll()
                 .and().apply(securityConfigurerAdapter())
