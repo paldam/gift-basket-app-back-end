71ea5082e56a71bcfe5a40e5dac955e5ba1bc9f0
dami_dev <dami@onet.eu>
2017-10-07 23:47:36 +0200

    add OrderService

---
+++ src/main/java/com/damian/service/OrderService.java
@@ -0,0 +1,34 @@
+package com.damian.service;
+
+import com.damian.model.Customer;
+import com.damian.model.Order;
+import com.damian.repository.CustomerDao;
+import com.damian.repository.OrderDao;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OrderService {
+
+    private OrderDao orderDao;
+    private CustomerDao customerDao;
+
+    OrderService(OrderDao orderDao, CustomerDao customerDao){
+        this.orderDao = orderDao;
+        this.customerDao= customerDao;
+    }
+
+   @Transactional
+   public void createOrder(Order order){
+
+        Customer customer = order.getCustomer();
+        
+        if (customer.getCustomerId() != null){
+            orderDao.save(order);
+        }else{
+            Customer savedCustomer = customerDao.saveAndFlush(customer);
+            order.setCustomer(savedCustomer);
+            orderDao.save(order);
+        }
+    }
+}
