e3c56b77e02db2b44e32001aa0543446c0c998d5
dami_dev <dami@onet.eu>
2018-08-19 00:44:17 +0200

    add saveCustomer method to controller

--- src/main/java/com/damian/repository/CustomerDao.java
+++ src/main/java/com/damian/repository/CustomerDao.java
@@ -11,4 +11,6 @@ import java.util.List;
  */
 public interface CustomerDao extends JpaRepository<Customer,Long> {
     public List<Customer> findAllBy();
+
+
 }
--- src/main/java/com/damian/rest/CustomerController.java
+++ src/main/java/com/damian/rest/CustomerController.java
@@ -8,19 +8,17 @@ import com.damian.repository.AddressDao;
 import com.damian.repository.CustomerDao;
 import com.damian.repository.OrderDao;
 import com.damian.service.OrderService;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class CustomerController {
-
+    private static final Logger logger = Logger.getLogger(OrderController.class);
     private CustomerDao customerDao;
     private AddressDao addressDao;
     private OrderDao orderDao;
@@ -38,6 +36,13 @@ public class CustomerController {
     ResponseEntity<List<Customer>> getCustomers(){
         List<Customer> customerList = customerDao.findAllBy();
         return new ResponseEntity<List<Customer>>(customerList, HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @PostMapping("/customers")
+    ResponseEntity<Customer> saveCustomers(@RequestBody Customer customer){
+        Customer savedCustomer = customerDao.save(customer);
+        return new ResponseEntity<Customer>(savedCustomer, HttpStatus.CREATED);
     }
 
     
