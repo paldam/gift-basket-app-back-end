28101a3b9cc3afe5dabe889bda163a958f9b51b2
Damian <damianpaluch87@gmail.com>
2020-09-02 13:06:47 +0200

    add sorting

--- src/main/java/com/damian/domain/product/ProductSubTypeDao.java
+++ src/main/java/com/damian/domain/product/ProductSubTypeDao.java
@@ -1,10 +1,11 @@
 package com.damian.domain.product;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductSubTypeDao extends CrudRepository<ProductSubType,Long>  {
+public interface ProductSubTypeDao extends JpaRepository<ProductSubType,Long> {
         List<ProductSubType> findAll();
         Optional<ProductSubType> findBySubTypeId(Integer id);
 
